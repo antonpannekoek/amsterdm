89a80a177858373e09c430f14931dfc3a0af2da8
Evert Rol <@>
2025-12-23 16:37:20 +0100

    Provide a "bowtie" function
    This will calculate the 2d array for a Bowtie plot: samples versus DM.

--- src/amsterdm/core.py
+++ src/amsterdm/core.py
@@ -7,7 +7,7 @@ import numpy as np
 DMCONST = 1.0 / 2.41e-4
 
 
-__all__ = ["mask2d", "correct_bandpass", "dedisperse", "calc_intensity"]
+__all__ = ["mask2d", "measure_background", "correct_bandpass", "dedisperse", "calc_intensity", "bowtie"]
 
 
 def mask2d(data, rowids):
@@ -297,3 +297,109 @@ def calc_intensity(
         return intensity, extra
 
     return intensity
+
+
+def bowtie(
+    data: np.ndarray,
+    dm: tuple[float, float],
+    freqs: np.ndarray,
+    tsamp: float,
+    badchannels: set | list | np.ndarray | None = None,
+    datarange: tuple[float, float] = (0.3, 0.7),
+    ndm: int = 50,
+    trange: slice | EllipsisType = Ellipsis,
+) -> np.ndarray:
+    """
+    Create the data for a bowtie plot: varying DM versus time/samples
+
+    Parameters
+    ----------
+    data : np.ndarray
+        data containing freq on the y-axis and time on the x-axis
+
+    dm : tuple[float, float]
+        range of the dispersion measure: start and stop
+
+    freqs : np.ndarray
+        frequencies corresponding to the channel centers
+
+    tsamp : float
+        sampling time interval in seconds
+
+    badchannels : set | list | np.ndarray | None, default=None
+        numbers of channels to flag/ignore
+
+    datarange : tuple[float, float], default=(0.3, 0.7)
+        Sample fractions within which the burst is located (after
+        dedispersion): lower and upper fraction. The remainder area is used for
+        background determination.
+
+        This section applies to the full data range; ``trange`` is ignored for
+        the background calculation.
+
+        The background is calculcated once for the central DM (thus for
+        ``(dm[0] + dm[1]) / 2``), so the datarange should be for the particular
+        DM; all other DM samples use the same background value.
+
+    ndm : int, default=50
+        Number of DM samples along the y-axis
+
+    trange : slice | EllipsisType, default=Ellipsis
+        Section of data on the "samples" axis to be used.
+
+        For the background calculationt, the full data section is used.
+
+    Returns
+    -------
+    np.ndarray
+    """
+
+    dm_center = (dm[0] + dm[1]) / 2
+
+    # Copy the data, flag bad channels and get a background estimate for the central DM
+    # After that, perform a bandpass correction and dedisperse to the central DM
+    # This provides the starting point for varying DM
+    # This assumes linear XX and YY polarization channels
+
+    xx = np.ma.array(data[:, 0, :])
+    yy = np.ma.array(data[:, 1, :])
+
+    if badchannels:
+        rowids = (
+            list(badchannels)
+            if not isinstance(badchannels, (list, np.ndarray))
+            else badchannels
+        )
+
+        xx[:, rowids] = np.ma.masked
+        yy[:, rowids] = np.ma.masked
+
+    xx = dedisperse(xx.T, dm_center, freqs, tsamp).T
+    yy = dedisperse(yy.T, dm_center, freqs, tsamp).T
+
+    # Bandpass correction
+    mean, std = measure_background(xx, datarange)
+    xx = (xx - mean[None, :]) / std[None, :]
+    mean, std = measure_background(yy, datarange)
+    yy = (yy - mean[None, :]) / std[None, :]
+
+    # Limit the data range
+    xx = xx[trange, :]
+    yy = yy[trange, :]
+    # xx and yy are now flagged, bandpass-corrected and dedispersed at the mean DM
+    # This provides the starting point for the iteration through dmrange
+
+    # dmrange is relatively the mean DM
+    dmrange = np.linspace(dm[0], dm[1], ndm) - dm_center
+
+    tie = []
+    for dm in dmrange:
+        xxdd = dedisperse(xx.T, dm, freqs, tsamp).T
+        yydd = dedisperse(yy.T, dm, freqs, tsamp).T
+        stokesI = xxdd + yydd
+        stokesI = np.ma.filled(stokesI, np.nan)
+        lc = np.nansum(stokesI, axis=1)
+        tie.append(lc)
+    tie = np.vstack(tie)
+
+    return tie
