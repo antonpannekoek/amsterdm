535f87b317615d5fdc724c7988b8ff0b0d1ac5da
Evert Rol <@>
2026-01-14 12:48:31 +0100

    Move constants into a separate module

---
+++ src/amsterdm/constants.py
@@ -0,0 +1,3 @@
+# According to Nimmo et al 2022, 10.1038/s41550-021-01569-9,
+# this is also the value that is used in dspsr (digifil) and SFXC
+DMCONST = 1.0 / 2.41e-4
--- src/amsterdm/core.py
+++ src/amsterdm/core.py
@@ -2,9 +2,7 @@ from types import EllipsisType
 
 import numpy as np
 
-# According to Nimmo et al 2022, 10.1038/s41550-021-01569-9,
-# this is also the value that is used in dspsr (digifil) and SFXC
-DMCONST = 1.0 / 2.41e-4
+from .constants import DMCONST
 
 
 __all__ = [
